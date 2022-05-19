90bbd95938e0d68c0a19180a8f5307f727ffe8c1
Alexis Stone <alexislcstone@gmail.com>
2022-05-18 17:08:12 -0700

    Finished API and rendered products

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-
 
 const app = express();
 app.use(express.json());
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-
+require("dotenv").config();
 const path = require('path');
 
 module.exports = {
-  mode:'development',
+  mode: 'development',
   entry: path.join(__dirname, "client/src", "index.jsx"),
   output: {
     path: path.join(__dirname, "/client/dist"),
