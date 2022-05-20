27b25814ffaff785d1336df6a4116f59b4e62e3d
zebibg <zgebres@gmail.com>
2022-05-20 07:51:16 -0700

    Added a ProductView Component

---
+++ client/src/components/ProductView.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import API from '../../API';
+import { useEffect, useState } from 'react';
+
+const ProductView =(props)=> {
+    return (
+        <div>
+        </div>
+
+    )
+}
+export default ProductView;
