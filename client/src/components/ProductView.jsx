c2c1e335d5aa0384cb7ac4d59122dbf92c8cf034
zebibg <zgebres@gmail.com>
2022-05-20 16:34:41 -0700

    Clicking on a photo changes the main image to the selected one

--- client/src/components/ProductView.jsx
+++ client/src/components/ProductView.jsx
@@ -6,11 +6,12 @@ const ProductView =(props)=> {
   const photos = props.selectedStyle.photos;
   const [selectedIndex, setSelectedIndex] = useState(0);
 
+
     return (
         <div style={{display:'flex', flexWrap: 'wrap', flexDirection: 'row'}}>
         <div style={{display:'flex', flexDirection: 'column'}}>
           {photos.map((photo, index)=>(
-            (selectedIndex !== index)? <div  key={index}><img src={photo.thumbnail_url} width="100"/></div>:null
+            (selectedIndex !== index)? <img src={photo.thumbnail_url} key={index} width="100" onClick={()=>setSelectedIndex(index)}/>:null
             ))}
         </div>
           {<img src={photos[selectedIndex].thumbnail_url}/>}
